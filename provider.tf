d1d9ec56f45d906e9081582995a8c8e286d19388
Chun Wu <chun.wu@canada.ca>
2020-05-27 20:16:13 +0000

    Modifiying provider to support helm 3

--- provider.tf
+++ provider.tf
@@ -7,3 +7,34 @@ provider "helm" {
 
   kubernetes {}
 }
+
+provider "helm" {
+  version                           = "v1.2.1"
+  debug                             = ""
+  plugins_path                      = ""
+  registry_config_path              = ""
+  repository_config_path            = ""
+  repository_cache                  = ""
+  helm_driver                       = ""
+
+  kubernetes {
+    config_path                     = ""                    
+    host                            = ""
+    username                        = ""
+    password                        = ""
+    token                           = ""
+    insecure                        = ""
+    client_certificate              = ""
+    client_key                      = ""
+    cluster_ca_certificate          = ""
+    config_context                  = ""
+    load_config_file                = ""
+
+    exec {
+      api_version                   = ""
+      command                       = ""
+      args                          = ""
+      env                           = ""
+    }
+  }
+}
